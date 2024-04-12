077e59e953e4da411f596e7016c914c9e88c0d15
Lesego Pitsi <l.quincypitsi@gmail.com>
2024-04-13 00:29:32 +0200

    Added the order feature, still a W.I.P

---
+++ sanity/schemaTypes/featured.ts
@@ -0,0 +1,26 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'dish',
+  title: 'Dish',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name of dish',
+      type: 'string',
+    },
+    {
+      name: 'short_description',
+      title: 'Short Description',
+      type: 'string',
+      validation: (Rule) => Rule.max(200),
+    },
+    {
+      name: 'restaurant',
+      title: 'Restaurant)',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'restaurant'}]}],
+    },
+  ],
+})
--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,7 @@
-import blockContent from './blockContent'
 import category from './category'
-import post from './post'
-import author from './author'
 
-export const schemaTypes = [post, author, category, blockContent]
+import restaurant from './restaurant'
+import dish from './dish'
+import featured from './featured'
+
+export const schemaTypes = [restaurant, category, dish, featured]
